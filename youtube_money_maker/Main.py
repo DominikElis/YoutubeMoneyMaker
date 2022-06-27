877c96abbe0acb34285e1163bed60416d85db15b
Dominik <Dominik.elis@hs-bochum.de>
2022-06-28 01:28:47 +0200

    +TelegramBot

--- youtube_money_maker/Main.py
+++ youtube_money_maker/Main.py
@@ -9,41 +9,54 @@ class youtube_money_maker:
         self.video_title = ""
 
     def downloadImage(self, url):
-        r = requests.get(url, allow_redirects=True)
-        open('picture.jpg', 'wb').write(r.content)
+        try:
+            r = requests.get(url, allow_redirects=True)
+            open('picture.jpg', 'wb').write(r.content)
+        except:
+            return False
+        return True
 
     def downloadMusic(self, url):
-        r = requests.get(url, allow_redirects=True)
-        title = url.split("filename=")[1]
-        index = title.rfind("-")
-        self.video_title = title[:index]
-        open('music.mp3', 'wb').write(r.content)
+        try:
+            r = requests.get(url, allow_redirects=True)
+            title = url.split("filename=")[1]
+            index = title.rfind("-")
+            self.video_title = title[:index]
+            open('music.mp3', 'wb').write(r.content)
+        except:
+            return False
+        return True
 
-    def add_static_image_to_audio(self, image_path, audio_path):
-        """Create and save a video file to `output_path` after
-        combining a static image that is located in `image_path`
-        with an audio file in `audio_path`"""
-        # create the audio clip object
-        audio_clip = AudioFileClip(filename=audio_path)
-        # create the image clip object
-        image_clip = ImageClip(image_path)
-        # use set_audio method from image clip to combine the audio with the image
-        video_clip = image_clip.set_audio(audio_clip)
-        # specify the duration of the new clip to be the duration of the audio clip
-        video_clip.duration = audio_clip.duration
-        # set the FPS
-        video_clip.fps = 1
-        # set the resolution
-        video_clip = video_clip.resize((1920, 1080)).add_mask()  # (image_clip.img.shape[1],image_clip.img.shape[0]))
+    def add_static_image_to_audio(self):
+        try:
+            """Create and save a video file to `output_path` after
+            combining a static image that is located in `image_path`
+            with an audio file in `audio_path`"""
+            # create the audio clip object
+            audio_clip = AudioFileClip(filename="music.mp3")
+            # create the image clip object
+            image_clip = ImageClip("picture.jpg")
+            # use set_audio method from image clip to combine the audio with the image
+            video_clip = image_clip.set_audio(audio_clip)
+            # specify the duration of the new clip to be the duration of the audio clip
+            video_clip.duration = audio_clip.duration
+            # set the FPS
+            video_clip.fps = 1
+            # set the resolution
+            video_clip = video_clip.resize((1920, 1080)).add_mask()  # (image_clip.img.shape[1],image_clip.img.shape[0]))
 
-        # write the resuling video clip
-        video_clip.write_videofile(f"OpenMusic - music for everyone [{self.video_title}].mp4")
-
+            # write the resuling video clip
+            file_name = f"OpenMusic - music for everyone [{self.video_title}].mp4"
+            video_clip.write_videofile(file_name)
+        except:
+            return False
+        return True, file_name
 
 if __name__ == '__main__':
     ymm = youtube_money_maker()
 
-    layout = [[sg.Text("Image URL"),sg.In(do_not_clear=False)],[sg.Text("Audio URL"),sg.In(do_not_clear=False)],[sg.Button("Generate Music Video")]]
+    layout = [[sg.Text("Image URL"), sg.In(do_not_clear=False)], [sg.Text("Audio URL"), sg.In(do_not_clear=False)],
+              [sg.Button("Generate Music Video")]]
     window = sg.Window(title="Youtube Money Maker", layout=layout, margins=(250, 250))
 
     while True:  # Event Loop
@@ -61,5 +74,3 @@ if __name__ == '__main__':
                 values[1] = ""
 
     window.Close()
-
-
---
+++ youtube_money_maker/TelegramBot.py
@@ -0,0 +1,110 @@
+import logging
+import time
+from threading import Thread
+
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler, InlineQueryHandler
+
+from youtube_money_maker.Main import youtube_money_maker
+
+# queue = []
+# ymm = youtube_money_maker()
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+async def new_music_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    arguments = update.message.text.split(" ")
+
+    image_url = arguments[1]
+    audio_url = arguments[2]
+
+    ymm = youtube_money_maker()
+
+    image_downloaded = ymm.downloadImage(url=image_url)
+    music_downloaded = ymm.downloadMusic(url=audio_url)
+
+    if image_downloaded and music_downloaded:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Download successful.")
+        music_video_generated, file_name = ymm.add_static_image_to_audio()
+        if music_video_generated:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Musicvideo is generated.")
+            await context.bot.send_video(chat_id=update.effective_chat.id,filename=file_name,video=open(file_name, 'rb'))
+        else:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="An error occured while generating.")
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="URLs are probably not correct.")
+    # queue.append((update.effective_chat.id,image_url,audio_url))
+    # generate_polling()
+
+
+
+# def generate_polling():
+#     while len(queue) != 0:
+#
+#
+#         job = queue.pop(0)
+#         ymm.downloadImage(job[1])
+#         ymm.downloadMusic(job[2])
+#
+#         print(f"JOB DELETED -> {job}")
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token("5437920544:AAHLdpsT0-rqncINFnxwAwRWgtytQCgoGPo").build()
+    job_queue = application.job_queue
+
+    # create Handlers
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
+    new_music_video_handler = CommandHandler('new_music', new_music_video)
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
+    # add Handlers
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(inline_caps_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(inline_caps_handler)
+    application.add_handler(new_music_video_handler)
+    application.add_handler(unknown_handler)
+
+    application.run_polling()
